d0f1e38bc0af476b9c6d92146a843ec1fafd1c37
Alvin Jeong <alvinjeong.98@gmail.com>
2020-08-22 00:03:28 -0700

    add completed project webpage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+
+// grid and list layout toggle
+
 let btn = document.getElementsByClassName('view-btn');
-let index;
+let index = 0;
 
 function button(n) {
     currentBtn(index = n);
@@ -15,10 +18,10 @@ function currentBtn(n) {
 }
 let sectionA = document.querySelector("#section-a");
 let images = document.querySelectorAll(".card-img-top");
-let media_lg = window.matchMedia("(min-width: 1200px)");
-let media_mob = window.matchMedia("(min-width: 598px)");
-let items = document.querySelectorAll("#items");
-let items_holder = document.querySelector("#items-holder");
+let mediaLarge = window.matchMedia("(min-width: 1200px)");
+let mediaMobile = window.matchMedia("(min-width: 598px)");
+let items = document.querySelectorAll(".items");
+let itemsHolder = document.querySelector("#items-holder");
 
 document.querySelector("#list-btn").addEventListener('click', function() {
     button(1);
@@ -28,17 +31,17 @@ document.querySelector("#list-btn").addEventListener('click', function() {
     }
     sectionA.style.display = "block";
 
-    if(media_lg.matches) {
+    if(mediaLarge.matches) {
         for (let i=0; i<items.length; i++) {
             items[i].style.width = "90%";
             items[i].style.margin = "1em";
-            items_holder.style.justifyContent = "center";
+            itemsHolder.style.justifyContent = "center";
         }
-    } else if(media_mob.matches) {
+    } else if(mediaMobile.matches) {
         for (let i=0; i<items.length; i++) {
             items[i].style.width = "97%";
             items[i].style.margin = "0em";
-            items_holder.style.justifyContent = "center";
+            itemsHolder.style.justifyContent = "center";
         }
     }
 
@@ -48,9 +51,63 @@ document.querySelector("#grid-btn").addEventListener('click', function() {
     button(0);
 
     for(let i=0; i<images.length; i++) {
-        images[i].style.display = ('');
+        images[i].style.display = ("");
         items[i].removeAttribute('style');
-        items_holder.removeAttribute('style');
+        itemsHolder.removeAttribute('style');
     }
     
 })
+
+
+// search function
+
+
+let input = document.querySelector("#search-input");
+let savedSection = document.querySelectorAll("ul li");
+const ULSECTION = document.querySelector("#items-holder");
+
+input.addEventListener('keyup', function() {
+    let typed = $(this).val();
+    let searchResults = search(typed);
+
+    document.querySelector("ul").innerHTML = "";
+    document.querySelector("ul").removeAttribute("style");
+    document.querySelector("footer").removeAttribute("style");
+
+    for (let i=0; i<searchResults.length; i++) {
+        document.querySelector("ul").appendChild(searchResults[i]);
+    }
+
+    if (typed == "") {
+        document.querySelector("ul").innerHTML = "";
+        for (let i=0; i<savedSection.length; i++) {
+            document.querySelector('ul').appendChild(savedSection[i]);
+        }
+    }
+
+    if (searchResults.length == 0) {
+        let noResultMsg = document.createElement('h4');
+        noResultMsg.textContent = "Nothing found!";
+        noResultMsg.style.margin = "3em";
+        noResultMsg.style.color = "#9c9c9c";
+        noResultMsg.style.fontWeight = "bold";
+        document.querySelector('ul').style.justifyContent = "center";
+        document.querySelector("ul").appendChild(noResultMsg);
+        document.querySelector("footer").style.position = "fixed";
+    }
+})
+
+function search(searchText) {
+    let displaySection = ULSECTION;
+    let resultArr = [];
+    searchText = searchText.toLowerCase();
+    
+
+    for (let i=0; i<displaySection.children.length; i++) {
+        if (displaySection.children[i].id.toLowerCase().includes(searchText)) {
+            resultArr.push(displaySection.children[i]);
+        } 
+    }
+
+    return resultArr;
+}
